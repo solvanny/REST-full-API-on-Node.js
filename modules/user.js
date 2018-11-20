f201d483153e8b037b15c2359d2dabe7e54fa8cd
Ion Soltan <ion.soltan@hotmail.com>
2018-11-19 22:13:51 -0500

    Implement authentication and authorization

---
+++ modules/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+
+
+const Users = mongoose.model('users', new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    minlength: 50,
+    required: true
+  },
+  email: {
+    type: String,
+    minlength: 2,
+    minlength: 255,
+    required: true,
+    unique:true
+  },
+  password: {
+    type: String,
+    minlength: 2,
+    minlength: 255,
+    required: true
+  }
+}));
+
+function validateUser(user) {
+  let userSchema = {
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().email({ minDomainAtoms: 2 }).required(),
+    password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required()
+  }
+  return Joi.validate(user, userSchema);
+}
+
+module.exports.Users = Users;
+module.exports.validation = validateUser;
