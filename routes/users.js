c0b67d91a678d8a18a65d54661f2dddd5c86c8ea
Ion Soltan <ion.soltan@hotmail.com>
2018-11-24 01:33:57 -0500

    Hashing  password

--- routes/users.js
+++ routes/users.js
@@ -1,60 +1,51 @@
+const bcrypt = require('bcrypt');
 const express = require('express');
+const _ = require('lodash');
 const router = express.Router();
 const { User, validation } = require('../modules/user');
 
 
 //Get all users
 router.get('/', async (req, res) => {
-  const users = await User.find().sort('name');
+  let users = await User.find().sort('name');
   res.send(users);
 });
 
 //Create a new user
 router.post('/', async (req, res) => {
-  const { error } = validation(req.body); 
+  let { error } = validation(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
-  let user = new User({ 
-    name: req.body.name,
-    password: req.body.password,
-    email: req.body.email
-   });
-  user = await user.save();
+  let user = await User.findOne({email: req.body.email});
+  if(user) res.status(400).send('User allredy registred!');
   
-  res.send(user);
+  user = new User(_.pick(req.body, ['name', 'password', 'email']));
+  let salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  await user.save();
+  res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 //Update user by ID
 router.put('/:id', async (req, res) => {
-  const { error } = validation(req.body); 
+  let { error } = validation(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
-
-  const user = await User.findByIdAndUpdate(req.params.id, { 
-    name: req.body.name,
-    password: req.body.password,
-    email: req.body.email
-  }, { new: true });
-
+  let user = await User.findByIdAndUpdate(req.params.id, _.pick(req.body, ['name', 'password', 'email']), { new: true });
   if (!user) return res.status(404).send('The user with the given ID was not found.');
-  
   res.send(user);
 });
 
 //Delete user by ID
 router.delete('/:id', async (req, res) => {
-  const user = await User.findByIdAndRemove(req.params.id);
-
+  let user = await User.findByIdAndRemove(req.params.id);
   if (!user) return res.status(404).send('The user with the given ID was not found.');
-
   res.send(user);
 });
 
 //Find user by ID
 router.get('/:id', async (req, res) => {
-  const user = await User.findById(req.params.id);
-
+  let user = await User.findById(req.params.id);
   if (!user) return res.status(404).send('The user with the given ID was not found.');
-
   res.send(user);
 });
 
